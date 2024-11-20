bdbb84aacefa86a9417e91392af79d11716fd60f
Michał Truszel <33067782+nnunurs@users.noreply.github.com>
2024-11-20 17:20:01 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import tailwindcssAnimated from "tailwindcss-animated";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("tailwindcss-animated")],
+  plugins: [daisyui, tailwindcssAnimated],
 };
